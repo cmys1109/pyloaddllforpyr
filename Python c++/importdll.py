aaaf11a6a1a6b8739efc67fa897afc2d796b764a
SeaIce <63960170+SeaIce233@users.noreply.github.com>
2020-12-24 03:03:17 +0800

    Update importdll.py

--- Python c++/importdll.py
+++ Python c++/importdll.py
@@ -1,10 +1,10 @@
 from ctypes import *
 
-work = CDLL("D:\cmys1109\Python c++\Dll1.dll")
+work = CDLL("pydll/*.dll")
 
 print(work)
 
 work.outloaded()
 
-a = work.outadd(5,999)
-print(a)
+out = work.outadd(5,999)
+print(out)
